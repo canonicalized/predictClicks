5f7a6d2f1bdbce2615e2e7a78a08db08c15f5d97
canonicalized <office@canonicalized.com>
2017-05-17 18:07:30 +0300

    a

--- R/petmart.R
+++ R/petmart.R
@@ -6,7 +6,6 @@
 #' @import markovchain
 #' @export
 
-load("data/model.rda")
 petmart <- function(current_url){
    out <- try(predict(model$estimate, newdata = current_url), silent = TRUE)
    if(inherits(out, "try-error")){
